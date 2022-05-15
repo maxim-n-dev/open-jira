df26f55e26fd92372d9b3e43edfd089c32dedc92
Maksym Yuriy Nakonechnyy <maxim@MacBook-Air-de-Maksym.local>
2022-05-15 16:36:17 +0200

    added mongo connection and image configuration

---
+++ database/db.ts
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+
+
+/**
+ * 0 = disconnected
+ * 1 = connected
+ * 2 = connecting
+ * 3 = disconnecting
+ */
+
+const mongooConnection = {
+  isConnected: 0
+}
+
+export const connect = async () => {
+  if (mongooConnection.isConnected === 1) {
+    console.log(' Conectado ');
+    return;
+  } 
+
+  if( mongoose.connections.length > 0 ) {
+    mongooConnection.isConnected = mongoose.connections[0].readyState;
+
+    if( mongooConnection.isConnected === 1 ){
+      console.log('Usando conexión anterior.');
+      return;
+    }
+    await mongoose.disconnect();
+  }
+
+  await mongoose.connect('.....');
+  mongooConnection.isConnected = 1;
+
+  console.log('Connected to MongoDB: ', '...');
+
+}
+
+export const disconnect = async () => {
+  if(mongooConnection.isConnected !== 0) return;
+   
+  await mongoose.disconnect();
+  console.log('Desconectado de MongoDB');
+}
